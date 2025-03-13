6eb3b1c274a0370d48bfd5249bc315444d88cd44
MatheusVictorMoldsoft <Matheus.victor@moldsfot.email>
2025-03-13 10:50:29 -0300

    credenciais

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store';
 
 const API = axios.create({
-   baseURL: 'http://127.0.0.1:8000/api/v1',
+   baseURL: 'https://api.moldconsult.online/api/v1',
 });
 
 API.interceptors.request.use(
